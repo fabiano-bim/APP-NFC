42afa23fa4314782d4e03d6c4a2b725f6a53c9ad
fabiano-bim <fabiano.bim@hotmail.com>
2025-07-24 20:59:48 +0100

    Add files via upload
    git add gradlew.bat
    git commit -m "Adiciona gradlew.bat necessário para builds"
    git push

--- gradlew.bat
+++ gradlew.bat
@@ -1,5 +1,11 @@
 @echo off
 set DIR=%~dp0
-set APP_HOME=%DIR%
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-java -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+set JAVA_EXE=
+if exist "%JAVA_HOME%\bin\java.exe" set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+if not defined JAVA_EXE set JAVA_EXE=java
+
+"%JAVA_EXE%" ^
+  -Dorg.gradle.appname=%~n0 ^
+  -classpath "%DIR%\gradle\wrapper\gradle-wrapper.jar" ^
+  org.gradle.wrapper.GradleWrapperMain %*
+
