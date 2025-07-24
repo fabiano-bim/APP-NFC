a9174a43868dbca52f2ae4cebb759c1303746b6a
fabiano-bim <fabiano.bim@hotmail.com>
2025-07-24 21:00:15 +0100

    Add files via upload

---
+++ gradle/wrapper/gradlew.bat
@@ -0,0 +1,11 @@
+@echo off
+set DIR=%~dp0
+set JAVA_EXE=
+if exist "%JAVA_HOME%\bin\java.exe" set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+if not defined JAVA_EXE set JAVA_EXE=java
+
+"%JAVA_EXE%" ^
+  -Dorg.gradle.appname=%~n0 ^
+  -classpath "%DIR%\gradle\wrapper\gradle-wrapper.jar" ^
+  org.gradle.wrapper.GradleWrapperMain %*
+
